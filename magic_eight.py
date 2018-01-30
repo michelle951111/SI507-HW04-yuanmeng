b7b61fa662c2f52cc0667e312481dd7e74e191d8
michelle951111 <yuanmeng@umich.edu>
2018-01-30 15:28:48 -0500

    check_question

--- magic_eight.py
+++ magic_eight.py
@@ -1,4 +1,16 @@
-def ask_question():
-    print("What is your question?")
+
+def question():
+	print("What is your question?")
 	question = input()
 	return question
+
+user_question = question()
+while(user_question!= "quit"):
+
+    if user_question.endswith("?"):
+        answer=print(random.choice(all_answers))
+
+    else:
+        print("I’m sorry, I can only answer questions.")
+
+    user_question = question()
